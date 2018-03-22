77e5363bef39a126b506defb3a727e2158879da9
ErikKarlkvist <erikkarlkvist95@gmail.com>
2018-03-22 16:22:23 +0100

    add travis stuff

--- FootballTownApp/App.js
+++ FootballTownApp/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>Changes you make will automatically .</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#24cdda',
     alignItems: 'center',
     justifyContent: 'center',
   },

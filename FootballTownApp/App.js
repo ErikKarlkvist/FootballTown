800cffb8e60453ca2a13df2a689ae611a33c18ba
ErikKarlkvist <erikkarlkvist95@gmail.com>
2018-03-25 17:21:50 +0200

    setup firebase

--- FootballTownApp/App.js
+++ FootballTownApp/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'react-native-firebase'
 
 export default class App extends React.Component {
   constructor(){
     super()
+    console.log(firebase)
   }
 
   render() {

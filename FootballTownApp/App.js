4fc19faa2c03788579e0c569b2c2e663fac395f6
ErikKarlkvist <erikkarlkvist95@gmail.com>
2018-03-27 14:25:56 +0200

    remove expo

--- FootballTownApp/App.js
+++ FootballTownApp/App.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import firebase from 'react-native-firebase'
 
 export default class App extends React.Component {
   constructor(){
-    super()
+      super()
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-      <Text>Hello World! </Text>
-      <Text></Text>
-
-      </View>
-    );
+      return (
+          <View style={styles.container}>
+              <Image style={styles.image} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/San_Francisco_Bay_Bridge_Western_Span_at_night.jpg'}}></Image>
+              <Text>{this.props.name}</Text>
+              <Text>Alex was here</Text>
+                  <Text>Testing git push</Text>
+              <Text></Text>
+          </View>
+      );
   }
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#24cdda',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#24cdda',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  image: {
+      width: 250,
+      height: 250,
   },
 });
---
+++ FootballTownApp/ios/Pods/Target Support Files/Pods-FootballTownApp/Pods-FootballTownApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FootballTownApp : NSObject
+@end
+@implementation PodsDummy_Pods_FootballTownApp
+@end

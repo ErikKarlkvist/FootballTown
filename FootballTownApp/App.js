4d4c3312355b70dbfe2697b72cfc11ccecc80985
ErikKarlkvist <erikkarlkvist95@gmail.com>
2018-03-27 13:50:18 +0200

    remove comp file

--- FootballTownApp/App.js
+++ FootballTownApp/App.js
@@ -5,13 +5,33 @@ import Comp from './comp.js'
 
 export default class App extends React.Component {
   constructor(){
-    super()
-    console.log(firebase)
+      super()
   }
 
   render() {
-    return (
-        <Comp name={"Demo Hello!"}></Comp>
-    );
+      return (
+          <View style={styles.container}>
+              <Image style={styles.image} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/San_Francisco_Bay_Bridge_Western_Span_at_night.jpg'}}></Image>
+              <Text>{this.props.name}</Text>
+              <Text>Alex was here</Text>
+                  <Text>Testing git push</Text>
+              <Text></Text>
+
+          </View>
+      );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '#24cdda',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  image: {
+      width: 250,
+      height: 250,
+  },
+});

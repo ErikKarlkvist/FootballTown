c2c96b2c16f68256c380ea5e580cddb0d29473d9
Tbbas <alldentobias@gmail.com>
2018-04-12 20:45:58 +0200

    Added news component

---
+++ FootballTownApp/src/component/NewsComponent.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import {AppRegistry, Text, FlatList, View, StyleSheet } from 'react-native';
+import Factory from '../database/Factory';
+import {Colors} from '../config/UIConfig'
+
+
+export default class NewsComponent extends Component {
+  constructor(props) {
+  	super(props);
+
+	this.state = {
+		loading: false,
+		page: 1,
+		errors: null,
+		refreshing: false,
+    news: Factory.getNewsInstance()
+	};
+
+  }
+
+  refresh = () => {
+    this.setState(
+      {
+        page: 1,
+        refreshing: true
+      }
+    );
+  };
+
+  handleLoadMore = () => {
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+        // Fetch more data
+      
+    );
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#FFF",
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return <Text style={styles.listTitle}>Recent News</Text>;
+  };
+
+  renderFooter = () => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 1,
+          borderColor: "#CED0CE"
+        }}
+      >
+      </View>
+    );
+  };
+
+  render() {
+    console.log(this.state.game)
+    return (
+        <FlatList
+          data={this.state.news.getNews()}
+          renderItem={({ item }) => (
+            <NewsStory title={item.title} text={item.text} />
+          )}
+          keyExtractor={item => item.id}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+    );
+  }
+}
+class NewsStory extends Component {
+  constructor(props) {
+    super(props);
+
+}
+
+// Returns the news text to be shown for a given article
+getExceptText() {
+  if(this.props.except != null) {
+    return this.props.except;
+  } else if(this.props.text.length < 200) {
+    return this.props.text
+  } else {
+    return (this.props.text.substring(0,200) + "...")
+  }
+}
+
+  render() {
+    return (
+      <View style={styles.newsStory}>
+       <Text style={styles.newsTitle}>{this.props.title}</Text>
+       <Text style={styles.newsText}>{this.getExceptText()}</Text>
+       </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  listTitle: {
+    marginTop: '6%',
+    textAlign: 'center',
+    fontSize: 22,
+    backgroundColor: Colors.Primary,
+    padding: 5,
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.PrimaryDark,
+  },
+  newsStory: {
+    alignItems: 'center',
+    backgroundColor: Colors.CardBackground,
+  },
+  newsTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: Colors.PrimaryText
+  },
+  newsText:{
+  color: 'gray',
+  fontSize: 12,
+  padding: '4%',
+  textAlign: 'left'
+  }
+});
--- FootballTownApp/src/database/News.js
+++ FootballTownApp/src/database/News.js
@@ -5,17 +5,21 @@ export default class News {
             {
                 id: "00001",
                 title: "Arsenal won",
-                text: "gg ez"
+                text: "Lorem Ipsum dolemar et",
+                except: "Arsenaö wins, it was a cool match"
             },
             {
                 id: "00002",
                 title: "United lost",
-                text: "too bad"
+                text: "United lost today, sorry"
             },
             {
                 id: "00003",
                 title: "Elfborg wins",
-                text: "5-0"
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fermentum, dui eget eleifend commodo, nisi dolor pretium nibh, ac sollicitudin nulla tellus ut elit. In elementum quis justo a ornare. Morbi laoreet placerat metus, tempor commodo elit laoreet eget. Quisque magna sem, facilisis nec commodo rutrum, viverra et lorem. Ut maximus iaculis metus, ut condimentum lorem interdum eget. Maecenas ac neque tellus. Vivamus ut leo odio. Pellentesque iaculis magna erat, nec venenatis enim feugiat et  "
+                 + "Aenean bibendum maximus purus eu sagittis. Nullam tempor nisi non odio volutpat sollicitudin. Nullam lacinia mi non ante viverra, nec blandit nisl pretium. Vivamus eleifend libero vel mauris tempor efficitur. Sed lobortis mauris ac magna posuere, sit amet dictum lorem luctus. Praesent volutpat, nulla ac molestie placerat, quam dolor iaculis ligula, et facilisis enim augue pretium velit. Nunc vitae augue dolor. Vestibulum ut nisl sodales, placerat lectus non, vestibulum diam. Maecenas consequat tempor quam, id pellentesque lectus porta a." 
+                 + "In magna lacus, malesuada hendrerit dapibus nec, luctus eget metus. Vestibulum id nunc et mi hendrerit varius. Morbi quis est laoreet, ultrices dui egestas, hendrerit nisl. Maecenas vehicula vehicula lobortis. Etiam imperdiet ac leo sit amet pellentesque. Aliquam erat volutpat. Duis lacinia lacinia lacus, ut finibus metus placerat eu. Sed in mauris id sapien fermentum facilisis ac sit amet libero. Maecenas elementum lacus nisl, quis facilisis",
+                
             }
         ]
     }

999e78c78bfa0cb25b5d49bae0659705c2122e02
Tbbas <alldentobias@gmail.com>
2018-04-18 12:38:24 +0200

    Base tab&stack navigator for feed page

---
+++ FootballTownApp/src/views/Feed_page.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View, 
+  Button,
+} from 'react-native';
+//import Icon from 'react-native-vector-icons/Ionicons'
+//MaterialIcons'
+
+class NewsFeed extends Component{
+  render() {
+  return (
+    <Text>THis is the news story feed</Text>
+  );
+  }
+}
+class NewsStory extends Component {
+  render() {
+    return(
+    <View>
+    return <Text>NewsStoryScreen</Text>
+    </View>
+    );
+  }
+}
+class EventsFeed extends Component {
+  render() {
+    return(
+    <View>
+      <Text>Events Screen</Text>
+      <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View> 
+      );
+  }
+}
+class EventScreen extends Component {
+  render() {
+    return(
+    <View>
+    <Text> Event screen </Text>
+    </View>
+    );
+  }  
+}
+
+
+
+// Stack navigator for the News stack 
+const NewsStack = StackNavigator({
+  NewsFeed: { screen: NewsFeed },
+  Details: { screen: NewsStory },
+});
+const EventStack = StackNavigator({
+  Events: { screen: EventsFeed },
+  Details: { screen: EventScreen },
+});
+export default TabNavigator(
+{
+  News: {screen: NewsStack},
+  Events: {screen: EventStack},
+
+},{});
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+
+  });

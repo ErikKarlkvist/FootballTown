a0030f7cb61be4c946d38103fb389abc3de59a99
Johan Ljungberg <johan.ljungberg@hotmail.com>
2018-04-24 15:47:13 +0200

    removed a small thing I forgot

--- FootballTownApp/src/views/Game_Details_page.js
+++ FootballTownApp/src/views/Game_Details_page.js
@@ -19,8 +19,6 @@ import {GlobalStyles} from '../config/UIStyleSheet';
   const imageHeight = Math.round(dimensions.width * 9 / 16);
   const screenWidth = dimensions.width;
 
-  text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-
 class Game_Details_page extends Component{
 
   constructor(props){

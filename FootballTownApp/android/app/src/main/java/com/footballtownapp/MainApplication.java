678f76a1b3114d1e37c8ffba13dd34223d517676
Phub71 <pooriya.balavi@gmail.com>
2018-04-25 18:01:59 +0200

    latest update

--- FootballTownApp/android/app/src/main/java/com/footballtownapp/MainApplication.java
+++ FootballTownApp/android/app/src/main/java/com/footballtownapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.footballtownapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnnestedscrollview.RNNestedScrollViewPackage;
+import com.rncollapsingtoolbar.RNCollapsingToolbarPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNNestedScrollViewPackage(),
+            new RNCollapsingToolbarPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
           new RNFirebaseFirestorePackage()
--- FootballTownApp/src/views/Events_page.js
+++ FootballTownApp/src/views/Events_page.js
@@ -15,6 +15,7 @@ import {Colors, Fonts} from '../config/UIConfig';
 import {GlobalStyles} from '../config/UIStyleSheet';
 
 
+
 const dimensions = Dimensions.get('window');
 const imageHeight = Math.round(dimensions.width * 9 / 16);
 const screenWidth = dimensions.width;
@@ -34,9 +35,9 @@ class Events_page extends Component{
             source={{uri: this.props.eventsStory.imageUrl}}
           />
           <View style={GlobalStyles.articleContainer}>
-            <Text style={GlobalStyles.title}>{this.props.eventsStory.title}</Text>
-            <Text style={GlobalStyles.ingress}>{this.props.eventsStory.ingress}</Text>
             <Text style={GlobalStyles.text}>{this.props.eventsStory.text}</Text>
+            <Text style={GlobalStyles.text}>Location: {this.props.eventsStory.location}</Text>
+            <Text style={GlobalStyles.text}>Price: {this.props.eventsStory.price}</Text>
 
           </View>
         </ScrollView>

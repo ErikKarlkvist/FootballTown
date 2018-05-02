0d9c6a3d85ea17d4e5f9ce5d7207b2aa5c4c49a9
ErikKarlkvist <erikkarlkvist95@gmail.com>
2018-05-02 14:51:47 +0200

    fix

--- FootballTownApp/src/component/TeamDesc.js
+++ FootballTownApp/src/component/TeamDesc.js
@@ -3,13 +3,10 @@ import {AppRegistry, Text, View, StyleSheet, Image } from 'react-native';
 import Factory from '../database/Factory';
 import {Colors} from '../config/UIConfig';
 
-
-
-
-
 export default class TeamDesc extends Component {
   constructor(props) {
-  	super(props);const Teams = Factory.getTeamsInstance();
+  	super(props);
+    //const Teams = Factory.getTeamsInstance();
 
     this.state = {
       loading: false,
@@ -21,51 +18,21 @@ export default class TeamDesc extends Component {
     };
 }
 
-componentDidMount(){
-  this.setState({loading: true})
-  Factory.getUserInstance().setFollowingTeam("Mx7bWLt3BsrwWEX4XCDn")
-  Factory.getUserInstance().getFollowingTeam().then((user) =>
-  {console.log(user)})
-  this.setState({
-      loading: false,
-      fetchedTeam: user,
+  componentDidMount(){
+    this.setState({loading: true})
+    //Factory.getUserInstance().setFollowingTeam("Mx7bWLt3BsrwWEX4XCDn")
+    Factory.getUserInstance().getFollowingTeam().then((team) => {
+      this.setState({
+        loading: false,
+        fetchedTeam: team,
+      })
+      console.log(user)
     })
   }
 
-
-
-  refresh = () => {
-    this.setState(
-      {
-        page: 1,
-        refreshing: true
-      }
-      // Fetch more data
-    );
-  };
-
-  handleLoadMore = () => {
-    this.setState(
-      {
-        page: this.state.page + 1
-      },
-        // Fetch more data
-
-    );
-  };
   render() {
     return (
-
-          data={this.state.fetchedTeam}
-          <View> team={data.name} </View>
-          keyExtractor={item => item.id}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-          ListFooterComponent={this.renderFooter}
-          onRefresh={this.handleRefresh}
-          refreshing={this.state.refreshing}
-          onEndReached={this.handleLoadMore}
-          onEndReachedThreshold={50}
+        <View><Text>{this.state.fetchedTeam.name}</Text></View>
     );
   }
 }

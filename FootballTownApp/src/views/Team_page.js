db18ad164681442e988679a3b5d7cafc18d08907
Phub71 <pooriya.balavi@gmail.com>
2018-05-03 14:25:55 +0200

    fixed team logo

--- FootballTownApp/src/views/Team_page.js
+++ FootballTownApp/src/views/Team_page.js
@@ -17,11 +17,6 @@ import Factory from '../database/Factory';
 
 
 
-let tempImage = "https://images.pexels.com/photos/39562/the-ball-stadion-football-the-pitch-39562.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
-let teamTitle = "Arsenal FC";
-let teamText = "Arsenal F.C. is a professional football club based in Islington, London, England, that plays in the Premier League, the top flight of English football. The club has won 13 League titles, a record 13 FA Cups, two League Cups, the League Centenary Trophy, 15 FA Community Shields, one UEFA Cup Winners' Cup and one Inter-Cities Fairs Cup. It is also the only English club to go a 38-match league season unbeaten, receiving the nickname The Invincibles, and a special gold Premier League trophy.";
-
-
 const dimensions = Dimensions.get('window');
 const imageHeight = Math.round(dimensions.width * 9 / 16);
 const screenWidth = dimensions.width;
@@ -75,18 +70,16 @@ export default class Team_page extends Component {
     const state = this.state;
     return (
       <ScrollView>
-
         <Image
         style={{width: screenWidth, height: imageHeight}}
         source={{uri: this.state.fetchedTeam.headerImage}}/>
 
-          <View >
-          <Text style={styles.teamTitle}>
+          <View style={styles.team}>
           <Image
           style={styles.flagStyle}
           source={{uri: this.state.fetchedTeam.flag}}/>
-          {this.state.fetchedTeam.name}</Text>
-          </View >
+          <Text style={styles.teamTitle}>{this.state.fetchedTeam.name}</Text>
+          </View>
           <View style={styles.container}>
           <Table borderStyle={{borderWidth: 0.5, borderColor: 'black'}}>
             <Row data={state.tableHead} style={styles.head} textStyle={styles.textHead}/>
@@ -110,8 +103,6 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingTop: 10,
 
-
-
         },
   head: {
      height: 40,
@@ -121,39 +112,34 @@ const styles = StyleSheet.create({
     margin: 6,
     textAlign: 'center',
     color: 'black'
-
+  },
+  team: {
+    flex: 0.3,
+    flexDirection: 'row',
+    height: 50,
+    display: 'flex',
+    margin: 10,
+    justifyContent: 'center',
   },
   textHead:{
-
     margin: 3,
     fontWeight: 'bold',
     textAlign: 'center',
     color: '#fff'
-
   },
   teamTitle:{
-
       color: Colors.PrimaryText,
       fontFamily: Fonts.Default,
       fontSize: 36,
       fontWeight: '400',
-      textAlign: 'center',
-
+      justifyContent: 'center',
   },
   flagStyle:{
-  alignItems: 'flex-start',
-    width: 120,
-    height: 120,
-
+    alignItems: 'flex-start',
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
   },
-  MainContainer :{
-
-// Setting up View inside content in Vertically center.
-justifyContent: 'center',
-flex:1,
-margin: 10
-
-},
 
 item: {
     padding: 10,

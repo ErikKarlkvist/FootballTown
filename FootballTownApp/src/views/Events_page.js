6f8092bdd1eb934ef8e1437927f634710220507b
Tbbas <alldentobias@gmail.com>
2018-05-03 16:08:26 +0200

    Added marker render on events

--- FootballTownApp/src/views/Events_page.js
+++ FootballTownApp/src/views/Events_page.js
@@ -6,15 +6,16 @@ import {
   View,
   ScrollView,
   Image,
-  Dimensions
+  Dimensions,
+  TouchableOpacity
 } from 'react-native';
 //MaterialIcons'
-import {TabNavigator} from 'react-navigation';
+import {TabNavigator,withNavigation} from 'react-navigation';
 import {Colors, Fonts} from '../config/UIConfig';
 import {GlobalStyles} from '../config/UIStyleSheet';
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import {openPositionInMapView} from '../navigation/MainNavigator'
 
 
 
@@ -38,13 +39,23 @@ class Events_page extends Component{
           />
           <View style={GlobalStyles.articleContainer}>
             <Text style={GlobalStyles.title}>{this.props.eventsStory.title}</Text>
-            <Text style={GlobalStyles.text}><Icon name='map-marker' size={15} style={{ marginLeft:'3%', padding: 10, color: "black" }}/> Location: {this.props.eventsStory.location}</Text>
+            <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('NearBy')}
+              >
+            <Text style={GlobalStyles.text}><Icon name='map-marker' size={15} style={{ marginLeft:'3%', padding: 10, color: "black" }}/> Location: {this.props.eventsStory.location.name}</Text>
+            </TouchableOpacity>
             <Text style={GlobalStyles.text}><Ionicons name='md-pricetags' size={13} style={{ marginLeft:'3%', padding: 10, color: "black" }}/> Price: {this.props.eventsStory.price}</Text>
             <Text style={GlobalStyles.text}>{this.props.eventsStory.text}</Text>
           </View>
         </ScrollView>
       );
     }
+
+    // Opens the given event in the Map tab. 
+    openEventInMap(event) {
+      this.props.navigation.navigate('NearBy',{event: event});
+    }
+
   }
 
-    export default Events_page;
+    export default withNavigation(Events_page);

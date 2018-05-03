ad034f9b2287b29d29c9410138a31f48c181f15f
Tbbas <alldentobias@gmail.com>
2018-05-03 08:35:58 +0200

    Added maps

--- FootballTownApp/src/views/Near_By_page.js
+++ FootballTownApp/src/views/Near_By_page.js
@@ -6,6 +6,7 @@
  */
 
 import React, { Component } from 'react';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import {
   Platform,
   StyleSheet,
@@ -17,13 +18,46 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import {TabNavigator} from 'react-navigation';
 
 class Near_By_page extends Component{
+  constructor(props) {
+    super(props);
+    this.state= {
+      // Change for town in which to hold cup
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers: [] // List of interesting places. 
+    }
+  }
   render() {
       return (
         <View style={styles.container}>
-        <Text>Welcome to maps, budds</Text>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          onRegionChange={this.onRegionChange}
+          region={this.state.region}
+          showsUserLocation = {true}
+
+        >
+        {this.state.markers.map(marker => (
+          <Marker 
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+            />
+          ))}
+
+        </MapView>
         </View>
       );
     }
+
+  changeRegion(region) {
+    this.setState({region})
+  }
   }
 
   export default Near_By_page;
@@ -31,15 +65,12 @@ class Near_By_page extends Component{
 
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 
   });

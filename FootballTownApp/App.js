9c339e304c537143e5d3d74f27174304c40e69de
ErikKarlkvist <erikkarlkvist95@gmail.com>
2018-05-21 10:17:47 +0200

    hype

--- FootballTownApp/App.js
+++ FootballTownApp/App.js
@@ -12,6 +12,7 @@ export default class App extends React.Component {
     super()
     Factory.getUserInstance().getAuthorization()
     Factory.getUserInstance().getFollowingTeam().then((team) => {console.log(team)})
+    console.disableYellowBox = true;
   }
 
   render() {
